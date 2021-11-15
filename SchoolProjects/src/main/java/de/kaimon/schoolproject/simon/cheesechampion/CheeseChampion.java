a0a90adda3d2f6803e156fc7d605f7575ee0a0dc
Asedem <asedem.games@gmail.com>
2021-11-15 12:26:28 +0100

    Mouse Project Start

---
+++ SchoolProjects/src/main/java/de/kaimon/schoolproject/simon/cheesechampion/CheeseChampion.java
@@ -0,0 +1,11 @@
+package de.kaimon.schoolproject.simon.cheesechampion;
+
+import de.kaimon.schoolproject.implementations.datenstrukturklassen.linear.Queue;
+import de.kaimon.schoolproject.implementations.datenstrukturklassen.linear.Stack;
+
+public class CheeseChampion {
+
+    private final Queue<Mouse> input = new Queue<>();
+    private final Queue<Mouse> output = new Queue<>();
+    private final Stack<Mouse> press = new Stack<>();
+}
