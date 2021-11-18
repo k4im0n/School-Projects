ad549801e9528bcec482c82236995c1fb5a838cf
Asedem <asedem.games@gmail.com>
2021-11-18 09:05:18 +0100

    CC-Created GUI

--- src/main/java/de/kaimon/schoolproject/simon/cheesechampiongui/GUI.java
+++ src/main/java/de/kaimon/schoolproject/simon/cheesechampiongui/GUI.java
@@ -1,6 +1,8 @@
 package de.kaimon.schoolproject.simon.cheesechampiongui;
 
-import sas.View;
+import sas.*;
+
+import java.awt.Color;
 
 public class GUI {
 
@@ -8,11 +10,15 @@ public class GUI {
 
     public GUI() {
         this.window = new View();
-
-
+        this.window.setSize(800, 600);
+        this.window.setName("CheeseChampion - By Simon Oberzier");
+        this.window.setBackgroundColor(Color.WHITE);
+        drawMap();
     }
 
     private void drawMap() {
-
+        new Rectangle(0, 240, 800, 60, Color.BLACK);
+        Rectangle rectangle = new Rectangle(200, 0, 60, 345, Color.BLACK);
+        rectangle.turn(200, 0, -30);
     }
 }
