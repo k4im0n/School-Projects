238c82993e286fa7a8d63a2bca5505baf0788995
kaigoe <kai.goe@gmx.de>
2022-01-10 12:02:34 +0100

    Sus

---
+++ src/main/java/de/kaimon/schoolproject/kai/fibonachi/Fibonachi.java
@@ -0,0 +1,19 @@
+package de.kaimon.schoolproject.kai.fibonachi;
+
+public class Fibonachi {
+
+    public Fibonachi(int n){
+        double first = 1;
+        double second = 1;
+        if(n>1) {
+            System.out.println("fib1: 1");
+            System.out.println("fib2: 1");
+            for (int i = 2 ; i<n; i++){
+                double temp = second;
+                second = first;
+                first = first + temp;
+                System.out.println("fib" + (i+1) + ": " + (int)(first+second));
+            }
+        } else second = 0;
+    }
+}
---
+++ src/main/java/de/kaimon/schoolproject/kai/fibonachi/FindFibonachi.java
@@ -0,0 +1,29 @@
+package de.kaimon.schoolproject.kai.fibonachi;
+
+public class FindFibonachi {
+    public FindFibonachi(int n){
+        if(n == 1 || n==2){
+            System.out.println("true");
+        } else{
+        double first = 1;
+        double second = 1;
+        if(n>=1) {
+            System.out.println("fib1: 1");
+            System.out.println("fib2: 1");
+            int i = 2;
+            while ((first + second) < n) {
+                double temp = second;
+                second = first;
+                first = first + temp;
+                System.out.println("fib" + (i + 1) + ": " + (int) (first + second));
+                i++;
+            }
+            if ((first + second) == n) {
+                System.out.println("true");
+            } else {
+                System.out.println("false");
+            }
+        }
+        }
+    }
+}
